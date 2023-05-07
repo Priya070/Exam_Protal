bd9345fd9cd6a25794a195622966b9f8a1595843
Priya <lcs2020055@iiitl.ac.in>
2023-05-07 16:33:15 +0530

    new

--- public/unwanted/login.js
+++ public/unwanted/login.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
         console.log('skdjfef')
-        let data = await fetch('https://examportal-0yo1.onrender.com/api/login', {
+        let data = await fetch('/api/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -16,16 +16,16 @@ window.addEventListener('load', () => {
 
         console.log(data)
         if (data === 'student') {
-            window.location.href = '/student'
+            window.location.href = 'student'
         }
         else if (data === "teacher") {
-            window.location.href = '/teacher'
+            window.location.href = 'teacher'
         }
         else if (data === 'admin') {
-            window.location.href = '/admin'
+            window.location.href = 'admin'
         }
         else {
-            window.location.href = '/login'
+            window.location.href = 'login'
         }
     }
 })
