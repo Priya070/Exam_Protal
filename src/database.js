e0dbfe21a1d1c9cdcf8ad8ec10d163d23eb7b49e
Priya <lcs2020055@iiitl.ac.in>
2023-05-07 13:09:54 +0530

    updated database.js

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const mongoUrl =
   "mongodb://0.0.0.0:27017/expo";
 
 const connectToMongo = mongoose
-  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect( process.env.MONGOURI || mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Error connecting to MongoDB", error));
 
